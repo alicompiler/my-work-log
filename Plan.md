58eabd5744ac58abac6bef8d3efdaf1ea8948f92
Ali Faris <alihcompiler@gmail.com>
2025-01-01 15:35:06 +0300

    setup firebase

---
+++ Plan.md
@@ -0,0 +1,8 @@
+# The Plan
+
+I will use this file as tracking system, i will register the steps/tasks that I need to do to complete the project.
+
+## Tasks
+
+- [DONE] Init the codebase
+- [DONE] Instal firebase
---
+++ src/Root/Settings/Firebase/Configuration.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyD_sJG2XzcA2JsrnlGZpLuEClbCJbOQrWE',
+    authDomain: 'my-work-log-38c6a.firebaseapp.com',
+    projectId: 'my-work-log-38c6a',
+    storageBucket: 'my-work-log-38c6a.firebasestorage.app',
+    messagingSenderId: '655539178217',
+    appId: '1:655539178217:web:ba33bfd09ef6cb1d587e78',
+};
